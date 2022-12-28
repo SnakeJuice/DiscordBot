266cb9b826f0f3f400c95c8e3b2158ecadb8baeb
SnakeJuice <khrizapellido@gmail.com>
2022-12-28 00:56:56 -0300

    updated token

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 import discord
 from discord.ext import commands
+from boto.s3.connection import S3Connection
 
 bot = commands.Bot(command_prefix="¿", intents=discord.Intents.all())
 
@@ -10,12 +11,12 @@ async def load_extensions():
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
 
+TOKEN = S3Connection(os.environ["TOKEN"])
 
 async def main():
     async with bot:
         await load_extensions()
         await bot.start(TOKEN)
-
 
 @bot.event
 async def on_ready():
